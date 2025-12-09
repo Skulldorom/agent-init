c9ce00a82caeb49ae7caae1109c3a3fa70b1810d
Mijack <61523653+MijackK@users.noreply.github.com>
2025-12-09 11:26:21 +0300

    Remove sudo from directory creation commands

--- init.sh
+++ init.sh
@@ -33,8 +33,8 @@ if [ ! -f "$ENV_FILE" ]; then
 fi
 
 #create project directory
-sudo mkdir -p "$PROJECT_DIR"
-sudo chmod 700 "$PROJECT_DIR"
+mkdir -p "$PROJECT_DIR"
+chmod 700 "$PROJECT_DIR"
 
 echo "Loading environment variables from $ENV_FILE..."
 
