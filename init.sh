f366a523d9fc78f2a808edd710dc35bee5ab520c
Mijack <61523653+MijackK@users.noreply.github.com>
2025-12-11 11:20:03 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -202,7 +202,7 @@ echo "✓ Docker services started"
 
 # initialize database
 echo "Initializing db..."
-if curl -X POST "http://localhost/api/init"; then
+if curl -X POST "$DOMAIN_NAME/api/init"; then
     echo "Backend initialized"
 else
     echo "Failed to initialize backend"
