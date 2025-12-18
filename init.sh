bab5bf7244886ef4c543bfb2705da6e0b80dc128
Mijack <61523653+MijackK@users.noreply.github.com>
2025-12-18 11:17:39 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -212,8 +212,9 @@ echo "✓ Docker services started"
 
 
 # initialize database
+
 echo "Initializing db..."
-if curl --retry 4 --retry-delay 2 --retry-connrefused -X POST "http://localhost/api/init"; then
+if curl --fail --retry 4 --retry-delay 2 --retry-connrefused --retry-all-errors -X POST "http://localhost/api/init"; then
     echo "Backend initialized"
 else
     echo "Failed to initialize backend"
